59b5011f2cee381232178731171ee06c9c25ea11
John Williamson <johnwilliamson@Johns-MacBook-Air-5.local>
2017-03-06 00:14:07 +1100

    firebase implementation

---
+++ react/happy-hour/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDsIFskbKCzS5PA7tiO9doaD9WRSSNjgrM",
+  authDomain: "happy-hour-1543a.firebaseapp.com",
+  databaseURL: "https://happy-hour-1543a.firebaseio.com"
+});
+
+export default base;
--- react/happy-hour/src/containers/bars.js
+++ react/happy-hour/src/containers/bars.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { fetchAllBars } from './../utils/bars';
 import Bar from './Bar';
+import base from '../base';
 
 const styles = {
   width: '90%',
@@ -13,29 +14,69 @@ class Bars extends Component {
   constructor(props){
     super(props);
     this.state = {
-       bars: [],
-       isLoading: true
+       bars: []
      }
+     this.fucksake = this.fucksake.bind(this);
   }
 
-  componentDidMount(){
-    fetchAllBars('http://localhost:3001/bars')
-    .then(bars => {
-      this.setState({ bars: bars, isLoading: false })
-    });
+
+  componentWillMount(){
+    this.ref = base.syncState('bars'
+    , {
+      context: this,
+      state: 'bars',
+      then: this.fucksake()
+    })
+  }
+
+
+  fucksake(){
+    console.log('fuck sake has run')
+    console.log(this.state.bars)
+    if (this.state.bars.length > 0) {
+      alert('Holy shit it worked.')
+    } else {
+      console.log('Still got nothing')
+    }
+  }
+
+  // shouldComponentUpdate(){
+  //   console.log('this is better')
+  //
+  //   if (this.state.bars.length > 0){
+  //     console.log('if thing ran')
+  //     fetchAllBars('http://localhost:3001/bars')
+  //     .then(bars => {
+  //       this.setState({ mergedBars: bars, isLoading: false })
+  //     });
+  //     console.log(this.state.mergedBars)
+  //     return true
+  //   } else {
+  //     console.log('something screwed up')
+  //   }
+  //   return true
+  // }
+
+  onChange(e){
+    this.setState({ bars: [ {
+    "deals" : "$4 shots",
+    "name" : "Montys Bar",
+    "place_id" : "ChIJZyorLT5D1moREoIW_ombT84",
+    "timeStart" : "6pm",
+    "timeEnd" : "7pm"
+  } ] })
   }
 
   render(){
 
-    if (this.state.isLoading) {
-      return <p>Loading…</p>;
-    }
+    console.log(this.state.bars);
+    console.log(this.state.mergedBars);
+
 
     return (
       <div>
-        {this.state.bars.map((bar, index) => {
-          return <Bar deals={bar.deals} key={index} data={bar} />
-        })}
+        <input type="text" onChange={(e) => this.onChange(e)} />
+
       </div>
     )
 
